3d8978ab082212e023b504e0c6f70b406d5357e1
Juancho Gonza <72740630+JuanchoGonza98@users.noreply.github.com>
2025-10-09 10:14:50 -0300

    Delete payments-service directory

--- payments-service/code/payment-service/src/main/java/com/ecommerce/paymentservice/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.paymentservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.ecommerce.paymentservice.model.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
